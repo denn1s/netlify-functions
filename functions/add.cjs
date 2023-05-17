74ffadbfdf9594c64eeeaa08ffa38d228ff919fe
Dennis Aldana <dennis.aldana@gmail.com>
2023-05-16 18:15:28 -0600

    fix: function must be async

--- functions/add.cjs
+++ functions/add.cjs
@@ -1,10 +1,10 @@
-module.exports.handler = (event) => {
+module.exports.handler = async (event) => {
   // event.body
   // event.queryStringParameters
   const { a, b } = event.queryStringParameters
 
   const sum = parseInt(a, 10) + parseInt(b, 10)
-  
+
   return {
     statusCode: 200,
     body: JSON.stringify({ sum }),
