84eb5a8a8fa5d0925f8dd8696d0cc4e30664ea79
Dennis Aldana <dennis.aldana@gmail.com>
2023-05-16 18:45:03 -0600

    fix: get pokemon await

--- functions/getPokemon.cjs
+++ functions/getPokemon.cjs
@@ -16,7 +16,7 @@ exports.handler = async () => {
 
   const conn = await pooll.getConnection()
 
-  const [rows] = conn.query("SELECT * FROM pokemon")
+  const [rows] = await conn.query("SELECT * FROM POKEMON")
 
   conn.release()
 
