f561b6bfe1518aec8c980408081e9801c044b760
Dennis Aldana <dennis.aldana@gmail.com>
2023-05-16 18:08:12 -0600

    add: add function

---
+++ api/add.cjs
@@ -0,0 +1,15 @@
+module.exports.handler = (event) => {
+  // event.body
+  // event.queryStringParameters
+  const { a, b } = event.queryStringParameters
+
+  const sum = parseInt(a, 10) + parseInt(b, 10)
+  
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ sum }),
+    headers: {
+      'Content-Type': 'application/json'
+    },
+  }
+}
