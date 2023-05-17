03ef3f30dd746fc3d67dd3507147fb3731a4c82a
Dennis Aldana <dennis.aldana@gmail.com>
2023-05-16 18:34:11 -0600

    add: getPokemon function

---
+++ functions/getPokemon.cjs
@@ -0,0 +1,30 @@
+const mysql = require('mysql2/promise')
+
+exports.handler = async () => {
+  const dbConfig = {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    ssl: {
+      rejectUnauthorized: true,
+    }
+  }
+
+  const pooll = mysql.createPool(dbConfig)
+
+  const conn = await pooll.getConnection()
+
+  const [rows] = conn.query("SELECT * FROM POKEMON")
+
+  conn.release()
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ data: rows }),
+    headers: {
+      'Content-Type': 'application/json'
+    },
+  }
+}
